0cac6c08c290854ae2c3ec552381e0d2a21f4d1f
Jonathan Dominguez <jonathanariel998@gmail.com>
2025-09-12 10:06:13 -0600

    Exposed pricing service as a microservice

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,14 +1,20 @@
 package com.udacity.pricing.domain.price;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
 import java.math.BigDecimal;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+
+@Entity
 public class Price {
 
     private String currency;
     private BigDecimal price;
+    @Id
     private Long vehicleId;
 
     public Price() {
